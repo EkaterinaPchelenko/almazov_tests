3b5be903ae8c244dd04134512dc23fa45218c61d
4218 Пчеленко Е.Ю <e.pchelenko@gmail.com>
2026-02-20 21:08:59 +0300

    Created project

---
+++ almazov_tests/backend/users/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        STUDENT = "student", "Student"
+        TEACHER = "teacher", "Teacher"
+        ADMIN = "admin", "Admin"
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.STUDENT
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
+
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="student_profile"
+    )
+
+    group = models.CharField(max_length=50)
+
+    overall_accuracy = models.FloatField(default=0)
+    total_tests_passed = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.email} profile"
